890cde4ed6a79153847480a0b8e801d07a4e8d47
Frederik Pytlick <frederikpyt@protonmail.com>
2023-10-06 21:31:50 +0200

    #16 - Updated context menu's

--- src/components/files/ContextMenu.vue
+++ src/components/files/ContextMenu.vue
@@ -1,32 +1,30 @@
 <template>
-	<div class="relative" v-on:contextmenu.prevent="openMenu">
-		<slot name="content"></slot>
-		<div
-			v-if="showMenu"
-			ref="menu"
-			v-on:blur="closeMenu"
-			tabindex="-1"
-			:style="{ top: `${top}px`, left: `${left}px` }"
-			class="absolute z-10 block w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"
-		>
-			<slot name="menu"></slot>
-		</div>
+	<div
+		v-if="showMenu"
+		ref="menu"
+		tabindex="-1"
+		:style="{ top: `${top}px`, left: `${left}px` }"
+		class="absolute z-10 block w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"
+	>
+		<slot></slot>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, nextTick } from 'vue';
+import { ref, nextTick } from 'vue';
+import { onClickOutside } from '@vueuse/core';
+import bus from '@lib/contextMenu';
 
 const menu = ref<HTMLElement | null>(null);
 const showMenu = ref(false);
 const top = ref(0);
 const left = ref(0);
 
-onMounted(() => console.log(menu.value));
+onClickOutside(menu, closeMenu);
 
-function setMenu(y: number, x: number) {
-	top.value = y;
+function setMenu(x: number, y: number) {
 	left.value = x;
+	top.value = y;
 }
 
 function closeMenu() {
@@ -38,10 +36,22 @@ function openMenu(e: MouseEvent) {
 
 	nextTick(() => {
 		if (!menu.value) return;
+		if (!e.currentTarget || !(e.currentTarget instanceof HTMLElement)) return;
 
-		menu.value.focus();
+		const currentTargetRect = e.currentTarget.getBoundingClientRect();
+		const offsetX = e.pageX - currentTargetRect.left;
+		const offsetY = e.pageY - currentTargetRect.top;
 
-		setMenu(e.offsetY, e.offsetX);
+		setMenu(offsetX, offsetY);
+
+		bus.off(closeMenu);
+		bus.emit();
+		bus.on(closeMenu);
 	});
 }
+
+defineExpose({
+	openMenu,
+	closeMenu,
+});
 </script>
--- src/components/files/Folder.vue
+++ src/components/files/Folder.vue
@@ -1,17 +1,19 @@
 <template>
-	<ContextMenu>
-		<template v-slot:content>
-			<a :href="url(`u/folder/${modelValue.id}`)">
-				<button
-					type="button"
-					:class="classes"
-					class="h-[40px] rounded-lg bg-gradient-to-br px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
-				>
-					{{ modelValue.name }}
-				</button>
-			</a>
-		</template>
-		<template v-slot:menu>
+	<div class="relative">
+		<a
+			:href="url(`u/folder/${modelValue.id}`)"
+			v-on:contextmenu.prevent="folderContextMenu?.openMenu"
+		>
+			<button
+				type="button"
+				:class="classes"
+				class="h-[40px] rounded-lg bg-gradient-to-br px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
+			>
+				{{ modelValue.name }}
+			</button>
+		</a>
+
+		<ContextMenu ref="folderContextMenu">
 			<ul class="py-2 text-sm text-gray-700 dark:text-gray-200">
 				<li>
 					<a
@@ -23,28 +25,37 @@
 				</li>
 				<li>
 					<a
-						href="#"
+						href="javascript:void(0)"
 						class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
 						>Change Name</a
+					>
+				</li>
+				<li>
+					<a
+						href="#"
+						class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+						>Share</a
 					>
 				</li>
 			</ul>
 			<div class="py-2">
 				<a
-					href="#"
+					href="javascript:void(0)"
 					class="block px-4 py-2 text-sm text-red-500 hover:bg-gray-100 dark:text-red-500 dark:hover:bg-gray-600"
 					>Delete</a
 				>
 			</div>
-		</template>
-	</ContextMenu>
+		</ContextMenu>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { type PropType, computed } from 'vue';
+import { type PropType, computed, ref } from 'vue';
 import { FolderClass } from '@lib/items/folders';
 import { url } from '@lib/helpers';
 import ContextMenu from './ContextMenu.vue';
+
+const folderContextMenu = ref<InstanceType<typeof ContextMenu>>();
 
 const props = defineProps({
 	modelValue: {
